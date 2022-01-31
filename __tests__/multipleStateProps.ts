a997867d8513187f363c8ede48badc4700231b57
Marcell Endrey <marcell.endrey@prezi.com>
2022-01-31 21:33:21 +0100

    tests

---
+++ __tests__/multipleStateProps.ts
@@ -0,0 +1,70 @@
+import { Transitions, Store } from '../src';
+
+type State = {
+    count: number;
+    numChanges: number;
+};
+
+type AppEvents = {
+    increase: (amount: number) => void;
+    decrease: (amount: number) => void;
+    reset: () => void;
+    multiplyThenAdd: (mult: number, add: number) => void;
+};
+
+const transitions: Transitions<State, AppEvents> = {
+    increase: amount => state => {
+        state.count += amount;
+        state.numChanges++;
+    },
+    decrease: amount => state => {
+        state.count -= amount;
+        state.numChanges++;
+    },
+    reset: () => state => {
+        state.count = 0;
+        state.numChanges++;
+    },
+    multiplyThenAdd: (mult, add) => state => {
+        state.count = state.count * mult + add;
+        state.numChanges++;
+    },
+};
+
+describe('state with multiple properties', () => {
+    test('initial state', () => {
+        const store = new Store(
+            {
+                count: 0,
+                numChanges: 0,
+            },
+            transitions,
+        );
+        expect(store.get()).toEqual({ count: 0, numChanges: 0 });
+    });
+
+    test('multiple props can change', () => {
+        const store = new Store(
+            {
+                count: 0,
+                numChanges: 0,
+            },
+            transitions,
+        );
+
+        store.dispatch.increase(10);
+        expect(store.get()).toEqual({ count: 10, numChanges: 1 });
+
+        store.dispatch.decrease(3);
+        expect(store.get()).toEqual({ count: 7, numChanges: 2 });
+
+        store.dispatch.reset();
+        expect(store.get()).toEqual({ count: 0, numChanges: 3 });
+
+        store.dispatch.increase(3);
+        expect(store.get()).toEqual({ count: 3, numChanges: 4 });
+
+        store.dispatch.multiplyThenAdd(3, 1);
+        expect(store.get()).toEqual({ count: 10, numChanges: 5 });
+    });
+});
--- __tests__/singleStateProp.ts
+++ __tests__/singleStateProp.ts
@@ -18,27 +18,33 @@ const transitions: Transitions<State, AppEvents> = {
     multiplyThenAdd: (mult, add) => state => void (state.count = state.count * mult + add),
 };
 
-const initialState: State = {
-    count: 0,
-};
+describe('state with single prop', () => {
+    test('initial state', () => {
+        const store = new Store({ count: 99 }, transitions);
+        expect(store.get().count).toBe(99);
+    });
 
-describe('store', () => {
-    test('works', () => {
-        const store = new Store(initialState, transitions);
+    test('events with single payload', () => {
+        const store = new Store({ count: 0 }, transitions);
 
         store.dispatch.increase(10);
         expect(store.get().count).toBe(10);
 
         store.dispatch.decrease(3);
         expect(store.get().count).toBe(7);
+    });
+
+    test('event with no payload', () => {
+        const store = new Store({ count: 10 }, transitions);
 
         store.dispatch.reset();
         expect(store.get().count).toBe(0);
+    });
 
-        store.dispatch.increase(3);
-        expect(store.get().count).toBe(3);
+    test('event with multipart payload', () => {
+        const store = new Store({ count: 2 }, transitions);
 
         store.dispatch.multiplyThenAdd(3, 1);
-        expect(store.get().count).toBe(10);
+        expect(store.get().count).toBe(7);
     });
 });

820e64456b218af1387a2ad605b8f8f368255ce4
Marcell Endrey <marcell.endrey@prezi.com>
2022-02-01 11:41:02 +0100

    side effects

---
+++ __tests__/sideEffect.test.ts
@@ -0,0 +1,53 @@
+import { createSideEffect, Store, Transitions } from '../src';
+
+type State = {
+    value: string;
+};
+
+type AppEvents = {
+    setValuePurely: (value: string) => void;
+    setValueWithSideEffect: (value: string) => any;
+    onlySideEffect: (value: string) => any;
+    zero: () => void;
+};
+
+const transitions: Transitions<State, AppEvents> = {
+    setValuePurely: value => state => void (state.value = value),
+    setValueWithSideEffect: value => state => {
+        state.value = value;
+        return sideEffects.consoleLogFoo();
+    },
+    onlySideEffect: () => () => sideEffects.consoleLogFoo(),
+    zero: () => () => {},
+};
+
+const sideEffects = {
+    consoleLogFoo: createSideEffect<[], void, AppEvents>(
+        'consoleLogFoo',
+        () => new Promise(resolve => resolve()),
+        transitions.zero,
+        transitions.zero,
+    ),
+};
+
+describe('side effects', () => {
+    test('just set the value', () => {
+        const store = new Store({ value: 'initial' }, transitions);
+        store.dispatch.setValuePurely('set-from-test');
+        expect(store.get().value).toBe('set-from-test');
+    });
+
+    test('set the value with side effect', () => {
+        const store = new Store({ value: 'initial' }, transitions);
+        store.dispatch.setValueWithSideEffect('set-from-test');
+        expect(store.get().value).toBe('set-from-test');
+        expect(store.TEST__scheduledSideEffects).toEqual([sideEffects.consoleLogFoo()]);
+    });
+
+    test('side effect only', () => {
+        const store = new Store({ value: 'initial' }, transitions);
+        store.dispatch.onlySideEffect('set-from-test');
+        expect(store.get().value).toBe('initial');
+        expect(store.TEST__scheduledSideEffects).toEqual([sideEffects.consoleLogFoo()]);
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -18,15 +18,42 @@ type EventHandlers<AppEvents extends BaseAppEvents> = {
 
 type Subscriber<State extends BaseState> = (value: State) => void;
 
+type SideEffect<Args extends Array<any>, ReturnType, AppEvents> = {
+    name: string;
+    execute: (...payload: Args) => Promise<ReturnType>;
+    args: Args;
+    successEvent: AppEvents[keyof AppEvents];
+    failureEvent: AppEvents[keyof AppEvents];
+};
+
+export function createSideEffect<Args extends Array<any>, ReturnType, AppEvents>(
+    name: string,
+    execute: (...payload: Args) => Promise<ReturnType>,
+    successEvent: AppEvents[keyof AppEvents],
+    failureEvent: AppEvents[keyof AppEvents],
+) {
+    return (...args: Args) => ({
+        name,
+        execute,
+        args,
+        successEvent,
+        failureEvent,
+    });
+}
+
+type SideEffectList<AppEvents extends BaseAppEvents> = Array<SideEffect<any, any, AppEvents>>;
+
 export class Store<State extends BaseState, AppEvents extends BaseAppEvents> {
     private state: State;
     private readonly eventHandlers: EventHandlers<AppEvents>;
     private subscribers: Subscriber<State>[];
+    private scheduledSideEffects: SideEffectList<AppEvents>;
 
     constructor(initialState: State, transitions: Transitions<State, AppEvents>) {
         this.state = initialState;
         this.eventHandlers = this.getEventHandlers(transitions);
         this.subscribers = [];
+        this.scheduledSideEffects = [];
     }
 
     private getEventHandlers(transitions: Transitions<State, AppEvents>): EventHandlers<AppEvents> {
@@ -35,8 +62,15 @@ export class Store<State extends BaseState, AppEvents extends BaseAppEvents> {
             const typedEventName = eventName as keyof AppEvents;
             const eventHandler = transitions[typedEventName];
             tmp[typedEventName] = ((...payload: Parameters<typeof eventHandler>) => {
-                this.state = produce(this.state, eventHandler(...(payload as any)));
+                const sideEffectList: SideEffectList<AppEvents> = [];
+                this.state = produce(this.state, draftState => {
+                    const updaterForGivenPayload = eventHandler(...(payload as any));
+                    sideEffectList.push(updaterForGivenPayload(draftState as any) as any);
+                });
                 this.broadcast();
+                if (sideEffectList.length > 0) {
+                    this.scheduleSideEffects(sideEffectList);
+                }
             }) as any;
         }
         return tmp as EventHandlers<AppEvents>;
@@ -57,5 +91,25 @@ export class Store<State extends BaseState, AppEvents extends BaseAppEvents> {
 
     private broadcast() {
         this.subscribers.forEach(subscriber => subscriber(this.state));
+    }
+
+    private scheduleSideEffects(sideEffectList: SideEffectList<AppEvents>) {
+        this.scheduledSideEffects.push(...sideEffectList);
+    }
+
+    get TEST__scheduledSideEffects() {
+        return [...this.scheduledSideEffects];
+    }
+
+    private executeSideEffects() {
+        while (this.scheduledSideEffects.length > 0) {
+            const sideEffect = this.scheduledSideEffects.shift();
+            if (sideEffect !== undefined) {
+                sideEffect
+                    .execute(...sideEffect.args)
+                    .then(result => (this.dispatch[sideEffect.successEvent as any] as any)(result))
+                    .catch(error => (this.dispatch[sideEffect.failureEvent as any] as any)(error));
+            }
+        }
     }
 }

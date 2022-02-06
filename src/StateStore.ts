23aa4f4c233c51742b3a5c1cd95a5fe126769860
Marcell Endrey <marcell.endrey@prezi.com>
2022-02-06 21:19:48 +0100

    break down function further

--- src/StateStore.ts
+++ src/StateStore.ts
@@ -21,33 +21,40 @@ export class StateStore<State extends BaseState, AppEvents extends BaseAppEvents
         this.eventHandlers = this.getEventHandlers(transitions);
     }
 
-    // For every transition definition `payload? => state => sideEffect?`
-    // create an event handler `payload? => void`, which
-    // 1. updates the state of the store using Immer,
-    // 2. broadcasts this new state to the store's subscribers, and
-    // 3. executes the transition's side effects (if any).
     private getEventHandlers(transitions: Transitions<State, AppEvents>): EventHandlers<AppEvents> {
         const eventHandlers = {} as EventHandlers<AppEvents>;
         for (const eventName of Object.keys(transitions)) {
             // Workaround for the problem that `typeof Object.keys(foo) !== keyof foo`
             const typedEventName = eventName as keyof AppEvents;
             const transition = transitions[typedEventName];
-            eventHandlers[typedEventName] = (...payload: Parameters<AppEvents[typeof typedEventName]>) => {
-                const sideEffectList: SideEffectList<AppEvents> = [];
-                this.value = produce(this.value, draftState => {
-                    const updaterForGivenPayload = transition(...payload);
-                    const maybeSideEffect = updaterForGivenPayload(draftState as State);
-                    if (maybeSideEffect !== undefined) {
-                        sideEffectList.push(maybeSideEffect);
-                    }
-                });
-                this.broadcast();
-                if (sideEffectList.length > 0) {
-                    return this.executeSideEffects(sideEffectList);
-                }
-            };
+            eventHandlers[typedEventName] = this.getEventHandler(typedEventName, transition);
         }
         return eventHandlers as EventHandlers<AppEvents>;
+    }
+
+    // For a given transition definition `payload? => state => sideEffect?`
+    // create an event handler `payload? => void`, which
+    // 1. updates the state of the store using Immer,
+    // 2. broadcasts this new state to the store's subscribers, and
+    // 3. executes the transition's side effects (if any).
+    private getEventHandler(
+        eventName: keyof AppEvents,
+        transition: Transitions<State, AppEvents>[typeof eventName],
+    ) {
+        return (...payload: Parameters<AppEvents[typeof eventName]>) => {
+            const sideEffectList: SideEffectList<AppEvents> = [];
+            this.value = produce(this.value, draftState => {
+                const updaterForGivenPayload = transition(...payload);
+                const maybeSideEffect = updaterForGivenPayload(draftState as State);
+                if (maybeSideEffect !== undefined) {
+                    sideEffectList.push(maybeSideEffect);
+                }
+            });
+            this.broadcast();
+            if (sideEffectList.length > 0) {
+                return this.executeSideEffects(sideEffectList);
+            }
+        };
     }
 
     get dispatch() {

1947c0b94a2e2c6710db4cd46155cb607d28e4a3
Marcell Endrey <marcell.endrey@prezi.com>
2022-02-06 21:37:06 +0100

    more precise transition types

--- src/transitions.ts
+++ src/transitions.ts
@@ -5,8 +5,23 @@ export type BaseAppEvents = {
     [eventName: string]: (...args: any) => any;
 };
 
+type SideEffectTypeForEventName<
+    AppEvents extends BaseAppEvents,
+    eventName extends keyof AppEvents,
+> = SideEffect<
+    Parameters<ReturnType<ReturnType<AppEvents[eventName]>>>,
+    ReturnType<ReturnType<ReturnType<AppEvents[eventName]>>>,
+    AppEvents
+>;
+
+type TransitionTypeForEventName<
+    State extends BaseState,
+    AppEvents extends BaseAppEvents,
+    eventName extends keyof AppEvents,
+> = (
+    ...payload: Parameters<AppEvents[eventName]>
+) => (state: State) => void | SideEffectTypeForEventName<AppEvents, eventName>;
+
 export type Transitions<State extends BaseState, AppEvents extends BaseAppEvents> = {
-    [eventName in keyof AppEvents]: (
-        ...payload: Parameters<AppEvents[eventName]>
-    ) => (state: State) => void | SideEffect<any, any, AppEvents>;
+    [eventName in keyof AppEvents]: TransitionTypeForEventName<State, AppEvents, eventName>;
 };

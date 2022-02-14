4de3d1d905681615a403177c65b63dd3a108beba
Marcell Endrey <marcell.endrey@prezi.com>
2022-02-14 09:04:33 +0100

    tests

--- __tests__/pureTransitions.ts
+++ __tests__/pureTransitions.ts
@@ -2,7 +2,7 @@ import { createTransitions } from '../src/transitions';
 import { Store } from '../src/Store';
 
 describe('pure transitions', () => {
-    test('work', () => {
+    test('single state field, single parameter payload', () => {
         type State = { count: number };
         const transitions = createTransitions<State>()({
             plus: (amount: number) => state => void (state.count += amount),
@@ -20,5 +20,72 @@ describe('pure transitions', () => {
 
         dispatcher.reset();
         expect(store.get().count).toBe(0);
+    });
+
+    test('multiple state fields, single parameter payload', () => {
+        type State = {
+            count: number;
+            numChanges: number;
+        };
+        const initialState: State = {
+            count: 0,
+            numChanges: 0,
+        };
+        const transitions = createTransitions<State>()({
+            plus: (amount: number) => state => {
+                state.count += amount;
+                state.numChanges++;
+            },
+            minus: (amount: number) => state => {
+                state.count -= amount;
+                state.numChanges++;
+            },
+            reset: () => state => {
+                state.count = 0;
+                state.numChanges++;
+            },
+        });
+        const store = new Store<State>(initialState);
+        const dispatcher = store.getDispatcher()(transitions);
+        expect(store.get()).toEqual(initialState);
+
+        dispatcher.plus(10);
+        expect(store.get()).toEqual({ count: 10, numChanges: 1 });
+
+        dispatcher.minus(3);
+        expect(store.get()).toEqual({ count: 7, numChanges: 2 });
+
+        dispatcher.reset();
+        expect(store.get()).toEqual({ count: 0, numChanges: 3 });
+    });
+
+    test('multiple state fields, multiple parameters payload', () => {
+        type State = {
+            count: number;
+            text: string;
+        };
+        const initialState: State = {
+            count: 0,
+            text: '',
+        };
+        const store = new Store<State>(initialState);
+        const transitions = createTransitions<State>()({
+            set: (count: number, text: string) => state => {
+                state.count = count;
+                state.text = text;
+            },
+            reset: () => state => {
+                state.count = 0;
+                state.text = '';
+            },
+        });
+        const dispatcher = store.getDispatcher()(transitions);
+        expect(store.get()).toEqual(initialState);
+
+        dispatcher.set(99, "but a glitch ain't one");
+        expect(store.get()).toEqual({ count: 99, text: "but a glitch ain't one" });
+
+        dispatcher.reset();
+        expect(store.get()).toEqual({ count: 0, text: '' });
     });
 });

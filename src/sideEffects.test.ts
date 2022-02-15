7b1e2c439f07d3a888aa7da0456c9f0ea7439b0e
Marcell Endrey <marcell.endrey@prezi.com>
2022-02-15 10:39:12 +0100

    more type tests for side effect creation

--- src/sideEffects.test.ts
+++ src/sideEffects.test.ts
@@ -1,4 +1,4 @@
-import { createSideEffectInstance } from './sideEffects';
+import { createSideEffectInstance, createSideEffects } from './sideEffects';
 
 const noop = () => () => {};
 const sum = (nums: Array<number>) => nums.reduce((a, b) => a + b, 0);
@@ -36,4 +36,63 @@ createSideEffectInstance: {
         value => state => void (state.count = value),
         noop,
     ]);
+}
+
+sideEffectInstance: {
+    // OK: no params
+    const e1 = createSideEffectInstance<{}>()('name', [() => Promise.resolve(), noop, noop]);
+    e1();
+
+    // OK: matching param
+    const e2 = createSideEffectInstance<{}>()('name', [
+        (count: number) => Promise.resolve(count),
+        noop,
+        noop,
+    ]);
+    e2(99);
+
+    // Error: non-matching param
+    // @ts-expect-error
+    e2('foobar');
+
+    // Error: missing param
+    // @ts-expect-error
+    e2();
+}
+
+createSideEffects: {
+    const e = createSideEffects<{ count: number }>()({
+        noParams: [() => Promise.resolve(), noop, noop],
+        takesNumber: [(count: number) => Promise.resolve(count), noop, noop],
+        takesString: [(text: string) => Promise.resolve(text), noop, noop],
+    });
+
+    // OK: calling no params
+    e.noParams();
+
+    // Error: trying to pass unexpected param
+    // @ts-expect-error
+    e.noParams(10);
+
+    // OK: calling with number
+    e.takesNumber(99);
+
+    // OK: calling with string
+    e.takesString('foo');
+
+    // Error: calling with wrong type
+    // @ts-expect-error
+    e.takesNumber('string');
+
+    // Error: omitting argument
+    // @ts-expect-error
+    e.takesNumber();
+
+    // Error: too many arguments
+    // @ts-expect-error
+    e.takesNumber(1, 2, 3);
+
+    // Error: no such side effect was created
+    // @ts-expect-error
+    e.noSuchProperty();
 }
